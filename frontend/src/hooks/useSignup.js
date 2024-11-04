5b05b14c122fb4621e20b8c525031536260526f1
PDARSHIL312 <darshilp200300@gmail.com>
2024-11-04 21:16:17 +0530

    stable f/b2

---
+++ frontend/src/hooks/useSignup.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
+
+const useSignup = () => {
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
+
+  const signup = async ({
+    fullName,
+    username,
+    password,
+    confirmPassword,
+    gender,
+  }) => {
+    const success = handleInputErrors({
+      fullName,
+      username,
+      password,
+      confirmPassword,
+      gender,
+    });
+    if (!success) {
+      toast.error("Validation failed");
+      return;
+    }
+
+    setLoading(true);
+    try {
+      const res = await fetch("http://localhost:7000/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          fullName,
+          username,
+          password,
+          confirmPassword,
+          gender,
+        }),
+      });
+
+      const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+
+      //localStorage
+      localStorage.setItem("chat-user", JSON.stringify(data));
+      //context
+      setAuthUser(data);
+
+      console.log(data);
+    } catch (e) {
+      toast.error(e.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { signup, loading };
+};
+
+export default useSignup;
+
+function handleInputErrors({
+  fullName,
+  username,
+  password,
+  confirmPassword,
+  gender,
+}) {
+  if (!fullName || !username || !password || !confirmPassword || !gender) {
+    toast.error("Some form values are missing");
+    return false;
+  }
+
+  if (password !== confirmPassword) {
+    toast.error("Password does not match the confirmation password");
+    return false;
+  }
+
+  if (password.length < 6) {
+    toast.error("Password must be at least 6 characters");
+    return false;
+  }
+  return true;
+}
